aebc54719ccec910248338f6e291e6a056ea3714
Emil Jørgensen <48445971+Emilioso007@users.noreply.github.com>
2024-01-15 21:12:05 +0100

    Added titlescreen

--- src/ScreenClasses/Screens/StartMenu.java
+++ src/ScreenClasses/Screens/StartMenu.java
@@ -5,30 +5,35 @@ import ScreenClasses.Screen;
 import ScreenClasses.ScreenManager;
 import ScreenClasses.ScreenObjects.Button;
 import processing.core.PApplet;
+import processing.core.PImage;
 
-public class StartMenu extends Screen{
+public class StartMenu extends Screen {
 
     private PApplet p;
     private ArrayList<Button> buttons;
+
+    private PImage background;
 
     public StartMenu() {
         super();
         p = ScreenManager.p;
 
+        background = p.loadImage("Images/Backgrounds/TitleScreen.png");
+
         buttons = new ArrayList<Button>();
-        buttons.add(new Button(p.width / 2 - 100, p.height / 2 - 50, 200, 100, "Start", "startButton"));
-        buttons.add(new Button(p.width / 2 - 100, p.height / 2 + 100, 200, 100, "Exit", "exitButton"));
+        buttons.add(new Button(p.width - 300, p.height / 2 + 50, 200, 100, "Start", "startButton"));
+        buttons.add(new Button(p.width - 300, p.height / 2 + 160, 200, 100, "Exit", "exitButton"));
     }
 
     public void update() {
-        
-        for(Button button : buttons){
+
+        for (Button button : buttons) {
             button.update();
         }
 
-        for(int i = 0; i < buttons.size(); i++){
-            if(buttons.get(i).pressed){
-                switch(buttons.get(i).key){
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons.get(i).pressed) {
+                switch (buttons.get(i).key) {
                     case "startButton":
                         ScreenManager.changeScreen(new Game());
                         break;
@@ -44,7 +49,9 @@ public class StartMenu extends Screen{
     public void render() {
         p.background(0);
 
-        for(Button button : buttons){
+        p.image(background, 0, 0);
+
+        for (Button button : buttons) {
             button.render(p);
         }
 
